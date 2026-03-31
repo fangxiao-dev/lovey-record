46d26f264d86ac03f48b07d0dfd77ec1f68a8e81
Fang Xiao <dexxholic@outlook.com>
2026-03-31 15:59:15 +0200

    fix: initialize wx.cloud before using callContainer in MP-WEIXIN

--- frontend/App.vue
+++ frontend/App.vue
@@ -2,6 +2,12 @@
 	export default {
 		onLaunch: function() {
 			console.log('App Launch')
+			// #ifdef MP-WEIXIN
+			wx.cloud.init({
+				env: 'prod-5gpr9j0q7ae42bfd',
+				traceUser: true
+			})
+			// #endif
 		},
 		onShow: function() {
 			console.log('App Show')
